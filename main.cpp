38253e41d294c732537eed24b3eeef8193588a68
Sujas <ce1221047@iitd.ac.in>
2024-10-01 23:31:43 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#define ASIO_STANDALONE
+#include<asio.hpp>
+#include<asio/ts/buffer.hpp>
+#include<asio/ts/internet.hpp>
+using namespace std;
+int main() {
+	cout << "Working";
+	return 0;
+}
