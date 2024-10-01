62d31983ebb1bf55fd3d39f979d40041452fd814
Sujas <ce1221047@iitd.ac.in>
2024-10-02 01:24:52 +0530

    switched to async for proper response handling

--- main.cpp
+++ main.cpp
@@ -3,11 +3,29 @@
 #include<asio.hpp>
 #include<asio/ts/buffer.hpp>
 #include<asio/ts/internet.hpp>
-using namespace std::chrono_literals;
 using namespace std;
+static vector<char> vBuffer(1024*8); // basically 1 KiB (78 of these are enough to send people on moon)
+
+void DataStreaming(asio::ip::tcp::socket& socket) { // for continuous data streaming between client and socket in a non blocking way
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](error_code ec, size_t length)
+		{
+			if (!ec)
+			{
+				cout << "\n\n Length of Data Read :" << length << " bytes\n\n";
+				for (size_t i = 0; i < length;i++) {
+					cout << vBuffer[i];
+				}
+				DataStreaming(socket);
+			}
+		}
+	);
+}
 int main() {
 	asio::error_code ec;
 	asio::io_context context;
+	asio::io_context::work idleWork(context); //to ensure context is finished Executing before running in thread
+	thread threadContext = thread([&]() {context.run();});
 	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 8000);
 	asio::ip::tcp::socket socket(context);
 	socket.connect(endpoint, ec);
@@ -18,8 +36,9 @@ int main() {
 		cout << "Failed to Connect to Address:\n" << ec.message() << endl;
 	}
 	if (socket.is_open()) {
+		DataStreaming(socket); // starting to read data as soon as the connection is established, tho no data is going to be fetched without any request
 		string getRequest =
-			"GET /cpp HTTP/1.1\r\n"
+			"GET /index.html HTTP/1.1\r\n"
 			"HOST: sujas.tech\r\n"
 			"Connection: close\r\n\r\n";
 		
@@ -31,19 +50,11 @@ int main() {
 		else {
 			cout << "Failed to Send Request:\n" << ec.message() << endl;
 		}
-		this_thread::sleep_for(100ms);
-		size_t availableBytes = socket.available();
-		if (availableBytes >= 0) {
-			cout << "Available Bytes to Read: " << availableBytes << endl;
-			vector<char> vBuffer(availableBytes);
-			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()),ec);
-			for (auto c : vBuffer) {
-				cout << c;
-			}
-		}
-		else {
-			cout << "No Response Recieved" << endl;
-		}
 	}
+	using namespace chrono_literals;
+	this_thread::sleep_for(10000ms);
+	context.stop();
+	if (threadContext.joinable()) threadContext.join();
+
 	return 0;
 }
